95efe4b750d073fcc85580a5c2cafd6f8fe35309
GodefroyClair <godefroy.clair@gmail.com>
2013-07-18 23:04:17 +0200

    applications 2

---
+++ support/v4/view/ViewCompatHC.java
@@ -0,0 +1,16 @@
+package android.support.v4.view;
+
+import android.animation.ValueAnimator;
+
+class ViewCompatHC
+{
+  static long getFrameTime()
+  {
+    return ValueAnimator.getFrameDelay();
+  }
+}
+
+/* Location:           C:\Developpement\pkg-dex2jar.jar
+ * Qualified Name:     android.support.v4.view.ViewCompatHC
+ * JD-Core Version:    0.6.2
+ */
---
+++ support/v4/widget/ScrollerCompatIcs.java
@@ -0,0 +1,16 @@
+package android.support.v4.widget;
+
+import android.widget.Scroller;
+
+class ScrollerCompatIcs
+{
+  public static float getCurrVelocity(Scroller paramScroller)
+  {
+    return paramScroller.getCurrVelocity();
+  }
+}
+
+/* Location:           C:\Developpement\pkg-dex2jar.jar
+ * Qualified Name:     android.support.v4.widget.ScrollerCompatIcs
+ * JD-Core Version:    0.6.2
+ */
